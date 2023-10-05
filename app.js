3dd91b7976273d33c00a24f3d01e1991bc4adce0
Nicolás Aguilera <60054614+nicoaguilerapy@users.noreply.github.com>
2023-10-05 10:32:42 -0300

    Add methods

--- app.js
+++ app.js
@@ -1,12 +1,53 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 80;
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-  const fechaHoraServidor = new Date().toLocaleString();
+  const fechaHoraServidor = new Date().toLocaleString('es-ES', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZoneName: 'short'
+  });
   const respuesta = {
     estado: 'activo',
     fechahora: fechaHoraServidor,
+  };
+  res.json(respuesta);
+});
+
+// Ruta para el método POST
+app.post('/', (req, res) => {
+  const requestBody = req.body;
+  const respuesta = {
+    metodo: 'POST',
+    body: requestBody,
+  };
+  res.json(respuesta);
+});
+
+// Ruta para el método PUT
+app.put('/', (req, res) => {
+  const requestBody = req.body;
+  const respuesta = {
+    metodo: 'PUT',
+    body: requestBody,
+  };
+  res.json(respuesta);
+});
+
+// Ruta para el método DELETE
+app.delete('/', (req, res) => {
+  const requestBody = req.body;
+  const respuesta = {
+    metodo: 'DELETE',
+    body: requestBody,
   };
   res.json(respuesta);
 });
